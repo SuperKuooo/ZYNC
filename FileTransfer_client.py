6af4f3d7020554803d6150dc122b769a6e5a4bab
SuperKuo <jerrykuo820@gmail.com>
2019-07-02 11:05:24 +0200

    Server Establish

--- FileTransfer_client.py
+++ FileTransfer_client.py
@@ -3,6 +3,7 @@ import time
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+
 def main():
     print("Initializing...")
     while establish_tcp_connection():
@@ -14,30 +15,32 @@ def main():
     s.close()
     return 0
 
-def establish_tcp_connection(TCP_IP= "192.168.1.118", TCP_PORT = 6815):
-    #BUFFERSIZE = 1024
+
+def establish_tcp_connection(TCP_IP="192.168.1.118", TCP_PORT=6815):
+    # BUFFERSIZE = 1024
+
     try:
         s.connect((TCP_IP, TCP_PORT))
-    except:
+    except socket.error:
         print("Error: Failed to establish connection")
         return 1
-   
+
     print("Connection Established")
     return 0
 
+
 def send_file():
-    MESSAGE = "Hello, World!"
+    message = "Hello, World!"
+    s
     try:
-        s.send(MESSAGE)
-    except:
+        s.send(message)
+    except socket.error:
         print("Error: Failed to send message")
         return 1
-    
+
     print("Message Sent!")
     return 0
 
+
 if __name__ == "__main__":
     main()
-
-
-
